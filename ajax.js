bdc4b1c4bcdd5597c2f55ccaae5bba9731126cf5
vetsinen <vetsinen@gmail.com>
2017-10-18 20:06:11 +0300

    clearing with paging

--- ajax.js
+++ ajax.js
@@ -1,12 +1,13 @@
-var graph = require('fbgraph');
 // 'https://httpbin.org/get'
 // var url = "https://graph.facebook.com/v2.10/" + "593812360?access_token=1724235024316583|FserG_5N2mjJ75fKjTpEvyUgI_E";
 // var url =  "https://graph.facebook.com/v2.10/me?access_token=EAACEdEose0cBAAg45FZCMBpUjygXoy77QFCwDS15c0T4wduFnQUMHd7EhLvf8p2CYFAPQpmuzg8lQwWhVnMiaGHbpXyndSZA0ZBtPSZCxEv9wZChPIYhRnUD8Tf37aL7V5adfngkL3I0UElLC52cgMF9mCK2mDfXZBIcs8ZCXiZClHhQxewVSlcISqGvRJAnEMYktwmMG3RLFAZDZD";
 var graph = require('fbgraph');
-graph.setAccessToken("EAACEdEose0cBAAg45FZCMBpUjygXoy77QFCwDS15c0T4wduFnQUMHd7EhLvf8p2CYFAPQpmuzg8lQwWhVnMiaGHbpXyndSZA0ZBtPSZCxEv9wZChPIYhRnUD8Tf37aL7V5adfngkL3I0UElLC52cgMF9mCK2mDfXZBIcs8ZCXiZClHhQxewVSlcISqGvRJAnEMYktwmMG3RLFAZDZD");
+graph.setAccessToken("EAACEdEose0cBAP4vprsMGmPoP1kfZBFnxl4Os7sg23kbZCC5qDZB0yIIgbnRvbkg23QpOZAEi83ZALLo7plIzA2S9at1gquZA2nATy7nKR40YyVjmoPi5GwSCiAWBXkOHiZCHtasYz9YnXZAtsVaAPvvc1YE2cwzpi0LvgjIdlzeoEHrPZCmFimpYdwo4yjaw8qv35xn5T4vHcAZDZD");
 
-graph.get("593812360/events", function(err, res) {
-        console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
-    });
+graph.get("593812360/events?limit=2", function (err, res) {
+    console.log(res.data[0].name); // { id: '4', name: 'Mark Zuckerberg'... }
+    console.log(res.paging);
+});
+
 //curl -i -X GET \
 // "https://graph.facebook.com/v2.10/me?access_token=EAACEdEose0cBAAg45FZCMBpUjygXoy77QFCwDS15c0T4wduFnQUMHd7EhLvf8p2CYFAPQpmuzg8lQwWhVnMiaGHbpXyndSZA0ZBtPSZCxEv9wZChPIYhRnUD8Tf37aL7V5adfngkL3I0UElLC52cgMF9mCK2mDfXZBIcs8ZCXiZClHhQxewVSlcISqGvRJAnEMYktwmMG3RLFAZDZD"
