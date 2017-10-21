ae14eeca5223f4eeeee92b1958094da6af1dcca3
vetsinen <vetsinen@gmail.com>
2017-10-22 01:47:48 +0300

    events. step by step

--- ajax.js
+++ ajax.js
@@ -1,14 +1,11 @@
 // 'https://httpbin.org/get'
 // var url = "https://graph.facebook.com/v2.10/" + "593812360?access_token=1724235024316583|FserG_5N2mjJ75fKjTpEvyUgI_E";
 // var url =  "https://graph.facebook.com/v2.10/me?access_token=EAACEdEose0cBAAg45FZCMBpUjygXoy77QFCwDS15c0T4wduFnQUMHd7EhLvf8p2CYFAPQpmuzg8lQwWhVnMiaGHbpXyndSZA0ZBtPSZCxEv9wZChPIYhRnUD8Tf37aL7V5adfngkL3I0UElLC52cgMF9mCK2mDfXZBIcs8ZCXiZClHhQxewVSlcISqGvRJAnEMYktwmMG3RLFAZDZD";
-var graph = require('fbgraph');
-
-async function z() {
-
-}
+const graph = require('fbgraph');
 
 graph.setAccessToken("EAACEdEose0cBANMQxt26cTkRqcraZCMMLlDiIBES5vP21pRvzOsXn5Fnz917pexPxFdxhyReZB5MzrLfkuLfwDkmj8UvVMDDsNdi0lS2ZAxuM4N30IA1N7hf05CaSVfqwAJaZCJ4MxAKOSTQhbyp27s023Co9hU7ENVX8QYpzFLIzscWjZALwXClb1BkdJwkZD");
-url = "593812360"; //"593812360/events?limit=2";
+// const url = "593812360";
+const  url = "593812360/events?limit=1";
 async function asyncGraph(url) {
     return new Promise((resolve, reject) => {
         graph.get(url, function (err, res) {
@@ -19,11 +16,14 @@ async function asyncGraph(url) {
 
 // asyncGraph('fdfdf').then((rez)=>{console.log(rez)});
 // console.log(asyncGraph('fdfdf'));
-async function main() {
-    const x = await asyncGraph(url);
-    console.log(x);
+async function main(url) {
+    for (let i=10;i>0;i--){
+        let rez = await asyncGraph(url);
+        url = rez.paging.next;
+        console.log(rez.data[0].name);
+    }
 }
-main();
+main(url);
 
 // graph.get(url, function (err, res) {
 //     console.log(res.name); // { id: '4', name: 'Mark Zuckerberg'... }
---
+++ me-events.js
@@ -0,0 +1,21 @@
+const e = {
+    data:
+        [{
+            description: 'Джейрек Роббинс \n- тренер пиковой результативности, в 23 награжден Золотой Медалью за особые заслуги\n- сын основателя индустрии лайф-коучинга, Тони Роббинса\n- предприниматель\n- основатель и владелец 2-х компаний\n- автор книги: "Live IT!: Achieving Success By Living With Purpose"\nрасскажет о:\n- прерывании паттерна\n- Majors vs Minors\n- внутренних оппонентах и как им противостоять\n- идеальной модели развития компании\n- успешности действий - цели и задачи\nи о многом другом - http://leya.events/robbins/',
+            end_time: '2018-08-30T13:00:00+0300',
+            name: 'Измени свою жизнь на 360° / Стань лучшей версией себя',
+            place: [Object],
+            start_time: '2018-08-30T10:00:00+0300',
+            id: '1745636865709839',
+            rsvp_status: 'unsure'
+        }],
+    paging:
+        {
+            cursors:
+                {
+                    before: 'QVFIUm0wSUNXM19yXzFwTlhyU1RNbzhOYndrVmVqdVRFXzBKWFVMM3JGUVBRVS1kYmpBaEZAFRHlmeGlDUTVmUHBGRzNkeDRXdWFyQS1GVkd4Y0dqRW85N0ZAn',
+                    after: 'QVFIUm0wSUNXM19yXzFwTlhyU1RNbzhOYndrVmVqdVRFXzBKWFVMM3JGUVBRVS1kYmpBaEZAFRHlmeGlDUTVmUHBGRzNkeDRXdWFyQS1GVkd4Y0dqRW85N0ZAn'
+                },
+            next: 'https://graph.facebook.com/v2.5/593812360/events?access_token=EAACEdEose0cBANMQxt26cTkRqcraZCMMLlDiIBES5vP21pRvzOsXn5Fnz917pexPxFdxhyReZB5MzrLfkuLfwDkmj8UvVMDDsNdi0lS2ZAxuM4N30IA1N7hf05CaSVfqwAJaZCJ4MxAKOSTQhbyp27s023Co9hU7ENVX8QYpzFLIzscWjZALwXClb1BkdJwkZD&limit=1&after=QVFIUm0wSUNXM19yXzFwTlhyU1RNbzhOYndrVmVqdVRFXzBKWFVMM3JGUVBRVS1kYmpBaEZAFRHlmeGlDUTVmUHBGRzNkeDRXdWFyQS1GVkd4Y0dqRW85N0ZAn'
+        }
+};
