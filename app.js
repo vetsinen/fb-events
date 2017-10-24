15638bf5e516c134349d71ee2995d194534f8f90
vetsinen <vetsinen@gmail.com>
2017-10-24 16:32:01 +0300

    works as express-service

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+let geocoder = require('geocoder');
+let metro = require('./metro-locs');
+
+app.get('/', function (req, res) {
+    // res.send('Hello World!');
+    geocoder.geocode("вулиця Жилянського 35, Київ, Україна", function (err, data) {
+        // console.log(data.results[0]);
+        let out = ""+ metro.closestMetro(data.results[0].geometry.location.lat,
+            data.results[0].geometry.location.lng);
+        res.send(out);
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
--- paging.js
+++
@@ -1,9 +0,0 @@
-var p = {
-    cursors:
-        {
-            before: 'QVFIUm0wSUNXM19yXzFwTlhyU1RNbzhOYndrVmVqdVRFXzBKWFVMM3JGUVBRVS1kYmpBaEZAFRHlmeGlDUTVmUHBGRzNkeDRXdWFyQS1GVkd4Y0dqRW85N0ZAn',
-            after: 'QVFIUmNQeWZANdmpyLTJZAUl80YmljX2ZAabkplbHQzaWNDdC1iNXppekZAsMDhVSllrZADRZAMnhnTmlGVk4zcmJ1SGc4VlJZATi1UblRiWXFoVmRmTXRma3l0OWdB'
-        },
-    next: 'https://graph.facebook.com/v2.5/593812360/events?access_token=EAACEdEose0cBAP4vprsMGmPoP1kfZBFnxl4Os7sg23kbZCC5qDZB0yIIgbnRvbkg23QpOZAEi83ZALLo7plIzA2S9at1gquZA2nATy7nKR40YyVjmoPi5GwSCiAWBXkOHiZCHtasYz9YnXZAtsVaAPvvc1YE2cwzpi0LvgjIdlzeoEHrPZCmFimpYdwo4yjaw8qv35xn5T4vHcAZDZD&limit=25&after=QVFIUmNQeWZANdmpyLTJZAUl80YmljX2ZAabkplbHQzaWNDdC1iNXppekZAsMDhVSllrZADRZAMnhnTmlGVk4zcmJ1SGc4VlJZATi1UblRiWXFoVmRmTXRma3l0OWdB'
-};
-console.log(p.next);
